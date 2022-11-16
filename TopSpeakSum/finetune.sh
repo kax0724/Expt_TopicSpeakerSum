8e2a7940b550f7a407a52b6b77336ce49aa2d9c9
kai <kaimax0724@gmail.com>
2022-11-16 10:24:18 +0900

    added loss calc isnan for preventing nan

--- TopSpeakSum/finetune.sh
+++ TopSpeakSum/finetune.sh
@@ -17,13 +17,12 @@ python finetune.py \
     --n_val 1000 \
     --val_check_interval 0.25 \
     --label_smoothing 0.1 \
-    --lr_scheduler  \
     --adafactor \
     --max_source_length 512 \
     --max_target_length 100 \
     --task summarization \
-    --train_batch_size 16 \
-    --eval_batch_size 16 \
+    --train_batch_size 1 \
+    --eval_batch_size 1 \
     --n_train -1 \
     --n_val -1 \
     --n_test -1 \
@@ -31,11 +30,7 @@ python finetune.py \
     --logger_name wandb \
     --use_speaker_embeds \
     --partial_embed \
-    --num_train_epochs 100 \
     --model_name_or_path google/pegasus-xsum \
-    --save_top_k 5 \
-    --early_stopping_patience 50 \
-    --warmup_steps 10 \
     --speaker_embed_scale 10 \
     --val_max_target_length 100 \
     --test_max_target_length 100 \
