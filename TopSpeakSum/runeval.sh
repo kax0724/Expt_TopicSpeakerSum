691b88f3913b5ff787c68ae5262f73ac7e3bee68
kai <kaimax0724@gmail.com>
2022-11-24 23:22:02 +0900

    preparing for evaluation

---
+++ TopSpeakSum/runeval.sh
@@ -0,0 +1,11 @@
+# Add parent directory to python path to access lightning_base.py
+export PYTHONPATH="../":"${PYTHONPATH}"
+# the proper usage is documented in the README, you need to specify data_dir, output_dir and model_name_or_path
+
+DATA_DIR=./data/samsum_dataset3 \
+OUTPUT_DIR=./output/topspeak \
+
+python runeval.py \
+    model_dir $OUTPUT_DIR \
+    dataset_dir $DATA_DIR  \
+    $@
